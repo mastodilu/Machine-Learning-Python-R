1f6b02959f83ca57f4f5e3ef6267091abd48cd19
Di Lucchio Matteo <matteo.dilucchio@beta80group.it>
2019-07-15 16:47:07 +0200

    clustering gerarchico

--- 04 - Clustering/Section 25 - Hierarchical Clustering/hc.py
+++ 04 - Clustering/Section 25 - Hierarchical Clustering/hc.py
@@ -22,25 +22,33 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
+
+
+
 # Using the dendrogram to find the optimal number of clusters
 import scipy.cluster.hierarchy as sch
-dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward')) # linkage è l'algoritmo di clustering gerarchic
+                                                             # method è il metodo usato per trovare i cluster
+                                                             # ward minimizza la varianza nei cluster.
 plt.title('Dendrogram')
 plt.xlabel('Customers')
 plt.ylabel('Euclidean distances')
 plt.show()
 
+# il numero ottimale di clustser (ottenuto guardando i tagli delle linee verticali rispetto a quelle orizzontali) è 5
+
 # Fitting Hierarchical Clustering to the dataset
 from sklearn.cluster import AgglomerativeClustering
-hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
-y_hc = hc.fit_predict(X)
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward') # affinity = euclinean indica di usare la distanza euclidea per il calcolo della diversità dei punti nei cluster
+                                                                                       # linkage = ward minimizza la varianza nei cluster
+y_hc = hc.fit_predict(X) # contiene un array di numeri da 0 a 4, per indicare che il valore di indice i appartiene al cluster numero N
 
 # Visualising the clusters
-plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 50, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 50, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 50, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 50, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 50, c = 'magenta', label = 'Cluster 5')
 plt.title('Clusters of customers')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
